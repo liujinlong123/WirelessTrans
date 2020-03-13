373a3b4111c385f7a0ddb42fcbaf1ab01dc513b5
AidenWalker.liu <liujinlong@loostone.com>
2020-03-13 18:11:19 +0800

    添加申请权限部分

--- app/src/main/java/com/mk/wirelesstrans/data/Constant.kt
+++ app/src/main/java/com/mk/wirelesstrans/data/Constant.kt
@@ -1,5 +1,7 @@
 package com.mk.wirelesstrans.data
 
+import android.Manifest
+
 /**
  * @ClassName:      Constant
  * @Description:    com.mk.wirelesstrans.data
@@ -7,4 +9,30 @@ package com.mk.wirelesstrans.data
  * @CreateDate:     2020/03/13 15:05
  */
 object Constant {
+
+    interface Permission {
+        companion object {
+            val WIFI = arrayOf(
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.ACCESS_NETWORK_STATE)
+
+            // TODO need to update
+            val BLUETOOTH = arrayOf(
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.ACCESS_NETWORK_STATE)
+
+            val ALL = arrayOf(
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.ACCESS_NETWORK_STATE)
+        }
+    }
 }
--- app/src/main/java/com/mk/wirelesstrans/view/activity/BaseActivity.kt
+++ app/src/main/java/com/mk/wirelesstrans/view/activity/BaseActivity.kt
@@ -1,6 +1,14 @@
 package com.mk.wirelesstrans.view.activity
 
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.mk.wirelesstrans.MyApplication
+import com.mk.wirelesstrans.R
+import com.mk.wirelesstrans.data.Constant
 
 /**
  * @ClassName:      BaseActivity
@@ -9,4 +17,89 @@ import androidx.appcompat.app.AppCompatActivity
  * @CreateDate:     2020/03/13 15:02
  */
 abstract class BaseActivity : AppCompatActivity() {
+    companion object {
+        private const val MY_PERMISSION_REQUEST_CODE = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        accessibility()
+    }
+
+    open fun toast(id: Int, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(MyApplication.instance, id, duration).show()
+    }
+
+    open fun toast(content: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(MyApplication.instance, content, duration).show()
+    }
+
+    /*--------------------------------------------------------------------------------------------*/
+    /**
+     * 需要申请权限:
+     */
+    private fun accessibility() {
+        // 第 1 步: 检查是否有相应的权限
+        val isAllGranted = checkPermissionAllGranted(Constant.Permission.ALL)
+
+        // 如果要申请的权限全都拥有, 则直接执行代码
+        if (isAllGranted) {
+            doBackup()
+            return
+        }
+
+        // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
+        ActivityCompat.requestPermissions(this, Constant.Permission.ALL,
+            MY_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    /**
+     * 检查是否拥有指定的所有权限
+     */
+    private fun checkPermissionAllGranted(permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * 第 3 步: 申请权限结果返回处理
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            var isAllGranted = true
+            // 判断是否所有的权限都已经授予了
+            for (grant in grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false
+                    break
+                }
+            }
+            if (isAllGranted) { // 如果所有的权限都授予了, 则执行备份代码
+                doBackup()
+            } else {
+                // Permission Denied
+                toast(R.string.PERMISSION_DENIED)
+                finish()
+            }
+        }
+    }
+
+    /**
+     * 第 4 步: 操作
+     */
+    private fun doBackup() {
+    }
 }
