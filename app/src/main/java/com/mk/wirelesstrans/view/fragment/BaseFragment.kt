e64fb2e6d0141563a167b9b5e5d228f1bd173a83
AidenWalker.liu <liujinlong@loostone.com>
2020-03-18 19:32:51 +0800

    修改客户端界面

--- app/src/main/java/com/mk/wirelesstrans/view/fragment/BaseFragment.kt
+++ app/src/main/java/com/mk/wirelesstrans/view/fragment/BaseFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 abstract class BaseFragment : Fragment() {
     open fun initData() {}
 
-    abstract fun initView()
+    open fun initView() {}
 
     open fun initListener() {}
 }
--- app/src/main/java/com/mk/wirelesstrans/view/fragment/wifi/DirectClientFragment.kt
+++ app/src/main/java/com/mk/wirelesstrans/view/fragment/wifi/DirectClientFragment.kt
@@ -13,6 +13,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.mk.wirelesstrans.R
 import com.mk.wirelesstrans.data.Constant
 import com.mk.wirelesstrans.databinding.DirectClientFragmentBinding
 import com.mk.wirelesstrans.inf.DeviceActionListener
@@ -26,14 +27,17 @@ import com.mk.wirelesstrans.viewmodel.WifiDirectVM
  * @Author:         Aiden.liu
  * @CreateDate:     2020/03/13 20:54
  */
-class DirectClientFragment : BaseFragment() {
+class DirectClientFragment : BaseFragment(), View.OnClickListener {
 
     companion object {
         private const val TAG = "DirectConnectFragment"
     }
 
     private lateinit var binding: DirectClientFragmentBinding
-    private lateinit var model: WifiDirectVM
+    private val model: WifiDirectVM by lazy {
+        ViewModelProvider(activity as FragmentActivity).get(WifiDirectVM::class.java)
+    }
+
     private var info: WifiP2pInfo? = null
 
     override fun onCreateView(
@@ -49,16 +53,7 @@ class DirectClientFragment : BaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        initData()
         initListener()
-    }
-
-    override fun initData() {
-        model = ViewModelProvider(activity as FragmentActivity).get(WifiDirectVM::class.java)
-    }
-
-    override fun initView() {
-
     }
 
     override fun initListener() {
@@ -66,27 +61,11 @@ class DirectClientFragment : BaseFragment() {
             binding.deviceName.text = it.device?.deviceName
             binding.deviceAddress.text = it.device?.deviceAddress
             binding.deviceState.text = it.state
-
-            // 只要一进入该界面
-            connectDevice(it.device)
         })
 
-        // TODO 假装发消息喽
-        binding.cardInfo.setOnClickListener {
-            Log.v(TAG, "点击了一下 ------> 准备发送")
-
-            if (info == null) return@setOnClickListener
-
-            val transIntent = Intent(activity, DataTransService::class.java)
-            transIntent.apply {
-                action = Constant.DataTransIntent.ACTION_SEND_DATA
-                putExtra(Constant.DataTransIntent.EXTRAS_STRING, "哼哼哼 我来啦")
-                putExtra(Constant.DataTransIntent.EXTRAS_GROUP_OWNER_ADDRESS, info?.groupOwnerAddress?.hostAddress)
-                putExtra(Constant.DataTransIntent.EXTRAS_GROUP_OWNER_PORT, 8988)
-            }
-
-            context?.startService(transIntent)
-        }
+        binding.connect.setOnClickListener(this)
+        binding.disconnect.setOnClickListener(this)
+        binding.send.setOnClickListener(this)
 
         // 这时候已经连接上了
         model.wifiP2pInfo.observe(viewLifecycleOwner, Observer { info ->
@@ -96,6 +75,9 @@ class DirectClientFragment : BaseFragment() {
         })
     }
 
+    /**
+     * 连接服务端
+     */
     private fun connectDevice(device: WifiP2pDevice?) {
         if (device == null) return
 
@@ -105,9 +87,32 @@ class DirectClientFragment : BaseFragment() {
         (activity as DeviceActionListener).connect(config)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            // 点击连接到服务端
+            R.id.connect -> {
+                connectDevice(model.wifiDirectItem.value!!.device)
+            }
 
-        (activity as DeviceActionListener).disconnect()
+            // 点击断开连接
+            R.id.disconnect -> {
+                (activity as DeviceActionListener).disconnect()
+            }
+
+            // 点击发送数据
+            R.id.send -> {
+                if (info == null) return
+
+                val transIntent = Intent(activity, DataTransService::class.java)
+                transIntent.apply {
+                    action = Constant.DataTransIntent.ACTION_SEND_DATA
+                    putExtra(Constant.DataTransIntent.EXTRAS_STRING, "哼哼哼 我来啦")
+                    putExtra(Constant.DataTransIntent.EXTRAS_GROUP_OWNER_ADDRESS, info?.groupOwnerAddress?.hostAddress)
+                    putExtra(Constant.DataTransIntent.EXTRAS_GROUP_OWNER_PORT, 8988)
+                }
+
+                context?.startService(transIntent)
+            }
+        }
     }
 }
