ae8bfa87170c4e2589b754bfd6e2c72a318e991a
AidenWalker.liu <liujinlong@loostone.com>
2020-03-19 10:02:14 +0800

    添加监听变量

--- app/src/main/java/com/mk/wirelesstrans/broadcast/WifiDirectReceiver.kt
+++ app/src/main/java/com/mk/wirelesstrans/broadcast/WifiDirectReceiver.kt
@@ -4,7 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.NetworkInfo
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pGroup
+import android.net.wifi.p2p.WifiP2pInfo
 import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.mk.wirelesstrans.data.bean.WifiDirectItem
 import com.mk.wirelesstrans.util.CommonUtil
@@ -25,6 +29,10 @@ class WifiDirectReceiver constructor(
 
     companion object {
         private const val TAG = "WifiDirectReceiver"
+    }
+
+    private val model: WifiDirectVM by lazy {
+        ViewModelProvider(activity).get(WifiDirectVM::class.java)
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -61,18 +69,25 @@ class WifiDirectReceiver constructor(
             }
 
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
-                val networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO) as NetworkInfo
-                if (networkInfo.isConnected) {
-                    manager.requestConnectionInfo(channel) {
-                        val model = ViewModelProvider(activity)
-                            .get(WifiDirectVM::class.java)
-                        model.wifiP2pInfo.value = it
-                    }
-                }
+                val wifiP2pInfo = intent.getParcelableExtra<WifiP2pInfo>(WifiP2pManager.EXTRA_WIFI_P2P_INFO)
+                val networkInfo = intent.getParcelableExtra<NetworkInfo>(WifiP2pManager.EXTRA_NETWORK_INFO)
+                val wifiP2pGroup = intent.getParcelableExtra<WifiP2pGroup>(WifiP2pManager.EXTRA_WIFI_P2P_GROUP)
+
+                model.wifiP2pInfo.value = wifiP2pInfo
+                model.networkInfo.value = networkInfo
+                model.wifiP2pGroup.value = wifiP2pGroup
+
+                Log.v(TAG, "wifiP2pInfo ------> $wifiP2pInfo")
+                Log.v(TAG, "networkInfo ------> $networkInfo")
+                Log.v(TAG, "wifiP2pGroup ------> $wifiP2pGroup")
             }
 
             WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                val wifiP2pDevice = intent.getParcelableExtra<WifiP2pDevice>(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE)
 
+                model.wifiP2pDevice.value = wifiP2pDevice
+
+                Log.v(TAG, "wifiP2pDevice ------> $wifiP2pDevice")
             }
         }
     }
--- app/src/main/java/com/mk/wirelesstrans/viewmodel/WifiDirectVM.kt
+++ app/src/main/java/com/mk/wirelesstrans/viewmodel/WifiDirectVM.kt
@@ -1,5 +1,8 @@
 package com.mk.wirelesstrans.viewmodel
 
+import android.net.NetworkInfo
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pGroup
 import android.net.wifi.p2p.WifiP2pInfo
 import androidx.lifecycle.MutableLiveData
 import com.mk.wirelesstrans.data.bean.WifiDirectItem
@@ -15,5 +18,11 @@ class WifiDirectVM : BaseVM() {
 
     val wifiDirectItem = MutableLiveData<WifiDirectItem>()
 
+    // WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION
     val wifiP2pInfo = MutableLiveData<WifiP2pInfo>()
+    val networkInfo = MutableLiveData<NetworkInfo>()
+    val wifiP2pGroup = MutableLiveData<WifiP2pGroup>()
+
+    // WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION
+    val wifiP2pDevice = MutableLiveData<WifiP2pDevice>()
 }
